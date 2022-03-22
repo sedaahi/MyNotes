d82686920161ab9b7abeda7d8a7b8335005c6c89
sedaahi <ahiseda@gmail.com>
2022-03-22 12:44:34 +0300

    React bootstrap

--- my-notes-ui/src/App.js
+++ my-notes-ui/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container } from 'react-bootstrap';
 
+const API_URL = "https://localhost:5001/";
+
+//İlk amacımız: notları listele
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+
+    axios.get(API_URL + "api/Notes")
+    .then(function(response){
+      setNotes(response.data);
+    });
+  }, [])
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+      <h1 className='mt-3'>My Notes</h1>
+    <ul>
+      {notes.map((x,i)=><li key={i}>{x.title}</li>)}
+    </ul>
+    </Container>
     </div>
   );
 }
